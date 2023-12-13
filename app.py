6916ded56e5087f761afbdcd498a7dea235735c4
Hruthik <hruthik5372@gmail.com>
2023-12-13 22:19:59 +0530

    added a basic route app.route("/")

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ socketio.on_namespace(admin_routes('/admin'))
 socketio.on_namespace(client_routes('/client'))
 socketio.on_namespace(rescue_routes('/rescue'))
 
+@app.route("/")
+def hello_world():
+    print("it works")
+    return "Hi guys"
+
 if __name__ == '__main__':
     socketio.run(app,  host='0.0.0.0', port=80, debug=True, allow_unsafe_werkzeug=True)
